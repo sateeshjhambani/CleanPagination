4eae1daf0166900fa5e45662f9c328f9d07ed7b4
Sateesh <sateesh.jh@gmail.com>
2023-06-13 20:51:22 +0400

    Added the screen and di setup

--- app/src/main/java/com/sateeshjh/cleanpagination/MainActivity.kt
+++ app/src/main/java/com/sateeshjh/cleanpagination/MainActivity.kt
@@ -3,16 +3,31 @@ package com.sateeshjh.cleanpagination
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.sateeshjh.cleanpagination.presentation.BeerScreen
+import com.sateeshjh.cleanpagination.presentation.BeerViewModel
 import com.sateeshjh.cleanpagination.ui.theme.CleanPaginationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CleanPaginationTheme {
-                // A surface container using the 'background' color from the theme
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val viewModel = hiltViewModel<BeerViewModel>()
+                    BeerScreen(beers = viewModel.beerPagingFlow.collectAsLazyPagingItems())
+                }
             }
         }
     }
